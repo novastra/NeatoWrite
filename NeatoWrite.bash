e37e74edb3e4bf8588a7519aefd2b5b464c9b31a
Novastra <astraneosis+git@gmail.com>
2016-07-19 19:50:04 +0200

    Main commit
    Upload of the first version of the script

---
+++ NeatoWrite.bash
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+#Copyright (C) 2016 Nicolas Alliot under MIT license available at : https://github.com/novastra/NeatoWrite/blob/master/LICENSE
+
+#Setting instructions for writable characters. Use bash 4 for associatives array
+declare -A alphabet
+alphabet["M"]='echo "M" && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 500 500 200" > "$port" && sleep 3 && echo "setmotor 720 0 200" > "$port" && sleep 5 && echo "setmotor 400 400 200" > "$port" && sleep 2 && echo "setmotor 0 720 200" > "$port" && sleep 5 && echo "setmotor 400 400 200" > "$port" && sleep 2 && echo "setmotor 720 0 200" > "$port" && sleep 5 && echo "setmotor 500 500 200" > "$port" && sleep 3 && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 100 100 200" > "$port" && sleep 2'
+alphabet["R"]='echo "R" && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 500 500 200" > "$port" && sleep 2 && echo "setmotor 180 -180 200" > "$port" && sleep 2 && echo "setmotor 300 300 200" > "$port" && sleep 2 && echo "setmotor 720 0 200" > "$port" && sleep 5 && echo "setmotor 300 300 200" > "$port" && sleep 2 && echo "setmotor -270 270 200" > "$port" && sleep 2 && echo "setmotor 500 500 200" > "$port" && sleep 2 && echo "setmotor -90 90 200" > "$port" && sleep 2 && echo "setmotor 100 100 200" > "$port" && sleep 2'
+alphabet["I"]='echo "I" && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 500 500 200" > "$port" && sleep 2 && echo "setmotor 360 -360 200" > "$port" && sleep 2 && echo "setmotor 500 500 200" > "$port" && sleep 2 && echo "setmotor -180 180 200" > "$port" && sleep 1 && echo "setmotor 100 100 200" > "$port" && sleep 2'
+alphabet["P"]='echo "P" && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 500 500 200" > "$port" && sleep 2 && echo "setmotor 180 -180 200" > "$port" && sleep 2 && echo "setmotor 720 0 200" > "$port" && sleep 5 && echo "setmotor -360 360 200" > "$port" && sleep 2 && echo "setmotor 300 300 200" > "$port" && sleep 2'
+alphabet["O"]='echo "O" && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 0 1440 200" > "$port" && sleep 8 && echo "setmotor 180 -180 200" > "$port" && sleep 2 && echo "setmotor 300 300 200" > "$port" && sleep 2'
+alphabet[" "]='echo "Space" && echo "setmotor 500 500 200" > "$port" && sleep 2'
+alphabet["A"]='echo "A" && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 300 300 200" > "$port" && sleep 2 && echo "setmotor 720 0 200" > "$port" && sleep 5 && echo "setmotor 150 150 200" > "$port" && sleep 2 && echo "setmotor 180 -180 200" > "$port" && sleep 2 && echo "setmotor 150 150 200" > "$port" && sleep 2 && echo "setmotor 360 -360 200" > "$port" && sleep 2 && echo "setmotor 150 150 200" > "$port" && sleep 2 && echo "setmotor 180 -180 200" > "$port" && sleep 2 && echo "setmotor 150 150 200" > "$port" && sleep 2 && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 100 100 200" > "$port" && sleep 2'
+alphabet["B"]='echo "B" && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 500 500 200" > "$port" && sleep 2 && echo "setmotor 180 -180 200" > "$port" && sleep 2 && echo "setmotor 720 0 200" > "$port" && sleep 5 && echo "setmotor -360 360 200" > "$port" && sleep 2 && echo "setmotor 720 0 200" > "$port" && sleep 5 && echo "setmotor -360 360 200" > "$port" && sleep 2 && echo "setmotor 300 300 200" > "$port" && sleep 2'
+alphabet["C"]='echo "C" && echo "setmotor 500 500 200" > "$port" && sleep 2 && echo "setmotor -360 360 200" > "$port" && sleep 2 && echo "setmotor 720 0 200" > "$port" && sleep 5 && echo "setmotor 360 -360 200" > "$port" && sleep 2 && echo "setmotor 0 720 200" > "$port" && sleep 5 && echo "setmotor 100 100 200" > "$port" && sleep 2'
+alphabet["D"]='echo "D" && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 300 300 200" > "$port" && sleep 2 && echo "setmotor 180 -180 200" > "$port" && sleep 2 && echo "setmotor 720 0 200" > "$port" && sleep 5 && echo "setmotor -360 360 200" > "$port" && sleep 2 && echo "setmotor 300 300 200" > "$port" && sleep 2'
+alphabet["E"]='echo "E" && echo "setmotor 100 100 200" > "$port" && sleep 2 && echo "setmotor 0 1440 200" > "$port" && sleep 8 && echo "setmotor 100 100 200" > "$port" && sleep 2 && echo "setmotor 100 100 200" > "$port" && sleep 2'
+alphabet["F"]='echo "F" && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 500 500 200" > "$port" && sleep 2 && echo "setmotor 180 -180 200" > "$port" && sleep 2 && echo "setmotor 300 300 200" > "$port" && sleep 2 && echo "setmotor 360 -360 200" > "$port" && sleep 4 && echo "setmotor 300 300 200" > "$port" && sleep 2 && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 250 250 200" > "$port" && sleep 2 && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 150 150 200" > "$port" && sleep 2 && echo "setmotor 360 -360 200" > "$port" && sleep 4 && echo "setmotor 150 150 200" > "$port" && sleep 2 && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 250 250 200" > "$port" && sleep 2 && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 300 300 200" > "$port" && sleep 2'
+alphabet["G"]='echo "G" && echo "setmotor 500 500 200" > "$port" && sleep 2 && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 200 200 200" > "$port" && sleep 2 && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 200 200 200" > "$port" && sleep 2 && echo "setmotor -270 270 200" > "$port" && sleep 3 && echo "setmotor 1440 0 200" > "$port" && sleep 6 && echo "setmotor 360 -360 200" > "$port" && sleep 3 && echo "setmotor 0 1440 200" > "$port" && sleep 6 && echo "setmotor 270 -270 200" > "$port" && sleep 3 && echo "setmotor 200 200 200" > "$port" && sleep 2 && echo "setmotor 180 -180 200" > "$port" && sleep 2 && echo "setmotor 200 200 200" > "$port" && sleep 2 && echo "setmotor -180 180 200" > "$port" && sleep 2 && echo "setmotor 300 300 200" > "$port" && sleep 2'
+
+#Get the port automatically by 10min filter or allow to define manually
+if [ $(find /dev/tty* -maxdepth 1 -mmin -10) ]
+then
+	port=$(find /dev/tty* -maxdepth 1 -mmin -10)
+else
+	find /dev/tty* -maxdepth 1 -mtime -1
+	read -p "Could not find the last connected port, type the full path to the port (above are the last connected):" -e port
+fi
+
+#Initialize serial port
+stty -F "$port" cs8 9600 ignbrk -brkint -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
+sleep 2
+
+#Ensure that the port is ready
+echo "" > "$port"
+sleep 1
+
+#Start testmode
+echo "testmode on" > "$port"
+sleep 2
+
+#Play sound when ready
+echo "playsound 1" > "$port"
+echo "Ready to go"
+sleep 1
+
+read -p "Type text to write with your bot :" -e message
+
+#Loops to get each letter and get the set of corresponding commands to send to the bot
+for (( i=0; i<${#message}; i++ ))
+do
+	for x in "${!alphabet[@]}"
+	do
+		if [ "$x" == "${message:$i:1}" ]
+		then
+			eval "${alphabet[$x]}"
+		fi
+	done  
+done
+
+#Play sound when finished
+echo "playsound 2" > "$port"
+echo "Finished : shutting down"
+sleep 2
+
+#Turn testmode off
+echo "testmode off" > "$port"
--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# NeatoWrite
-Write text with a Neato vacuum bot
+NeatoWrite
+==========
+
+Write text with your Neato bot
+
+The code is in bash 4 (using associative arrays) and allow you to type a message that the bot will write.
+
+<h3>GNU LINUX</h3>
+The code is in pure bash 4 so it can be executed 'as is' by most of the recent distros of GNU LINUX.
+Simple type <pre>bash NeatoWrite.bash</pre>
+
+<h3>MAC OSX</h3>
+OS X uses Bash 3 so you need to update your bash. If you have brew, type this in your terminal : 
+<pre>brew update && brew install bash</pre>
+Once it is done you just have to execute the bash file in the 'Mac_OSX' directory. 
+The only difference between this file and the linux one come from the /dev/ path that differs. To connect and have a response on the serial port you need to use cu* and not tty* port. (More info on tty/cu for mac <a target="_blank" href="http://pbxbook.com/other/mac-tty.html">here</a>)
+
+<h3>Windows</h3>
+For Windows, first you will need to get the correct pilots here :
+<ul>
+<li><a target="_blank" href="http://www.neatoroboticsupdates.com/NeatoInstaller/NeatoUpdaterToolInstaller_x86.exe">Windows 32</a></li>
+<li><a target="_blank" href="http://www.neatoroboticsupdates.com/NeatoInstaller/NeatoUpdaterToolInstaller_x64.exe">Windows 64</a>/li>
+</ul>
+
+Then you have the choice : 
+<ul>
+<li>you can try to get a bash shell on Windows and execute the bash code</li>
+<li>you can download <a target="_blank" href="https://ttssh2.osdn.jp/index.html.en">Teraterm</a> which allows you to communicate with the bot. You can then create a macro (launch ttpmacro then choose the .ttl file). 
+I will put an example of a macro in the 'Windows' directory but at this point it is quite easier to install a Gnu-Linux distro imo because the ttl syntax is not very well known.</li>
+
+<h3>Hardware</h3>
+For the hardware part you need a mini-usb cable, a pen and a scotch tape to fix the pen on the back part of the bot.
+
+<h3>Software</h3>
+The script is quite simple : 
+It tries to get the last connected and corresponding serial port (last 10 mins) and to connect to it. 
+It it fails you have to type the path to the port
+Once it has succedeed, you have to type the string of characters you want to write.
+Be careful of not typing characters that are not împlemented yet. Be also warned of the fact that if you remove your usb cable before the end, the bot will not leave the TestMode correctly.
+
+<h3>Instructions and writing</h3>
+The full programmer's guide is available <a target="_blank" href="https://www.neatorobotics.com/resources/programmersmanual_20140305.pdf">here</a>
+The basic instruction used to move the bot is <pre>setmotor leftWheelDistance rightWheelDistance globalSpeed</pre>
+
+To turn, the good angle will depend of the use of the two wheels. 
+If you want to make a 360° turn on the actual spot you can use <pre>setmotor 360 -360 200</pre>
+If you want to make a 360° turn to draw a circular arc you will prefer <pre>setmotor 720 0 200</pre> in most cases. 
+
+<h3>Alphabet</h3>
+At the moment only some characters can be written : 
+ABCDEFG<del>H</del>I<del>J</del><del>K</del><del>L</del>M<del>N</del>OP<del>Q</del>R<del>S</del><del>T</del><del>U</del><del>V</del><del>W</del><del>X</del><del>Y</del><del>Z</del>
+<del>0</del><del>1</del><del>2</del><del>3</del><del>4</del><del>5</del><del>6</del><del>7</del><del>8</del><del>9</del>
+
+<h3>Todo list</h3>
+<ul>
+<li>Complete the set of character that can be written</li>
+<li>Add a check on the OS at the begining to merge the GNU LINUX and MAC versions</li>
+<li>Add the possibility to define an handicap for a wheel (mine has to be corrected by 5° for 1° on the right wheel)</li>
+</ul>
+
+<h3>License</h3>
+Licensed under MIT license, <a target="_blank" href="https://github.com/novastra/NeatoWrite/blob/master/LICENSE">check here</a>.
